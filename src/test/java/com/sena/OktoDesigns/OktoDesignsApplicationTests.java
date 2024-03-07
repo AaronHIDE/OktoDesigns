e108213a35b0408f4d912d7903274653d7e07091
AaronHIDE <aaronochoa0504@gmail.com>
2024-03-07 11:28:04 -0500

    v0.1_Inicializacion del repositorio del proyecto OktoDesings 07/febrero/2024

---
+++ src/test/java/com/sena/OktoDesigns/OktoDesignsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.OktoDesigns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OktoDesignsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
