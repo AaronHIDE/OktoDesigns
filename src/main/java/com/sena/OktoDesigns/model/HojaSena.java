56aa36b304c88141254af224cc13abf6dc244195
XenaEstupinan <xena040404@gmail.com>
2024-04-01 11:06:40 -0500

    V0.2_VistaPlantillaUsuario_01042024

--- src/main/java/com/sena/OktoDesigns/model/HojaSena.java
+++ src/main/java/com/sena/OktoDesigns/model/HojaSena.java
@@ -1,10 +1,15 @@
 package com.sena.OktoDesigns.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "Formato Sena")
 public class HojaSena {
-
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 }
